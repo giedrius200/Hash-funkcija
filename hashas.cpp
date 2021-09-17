d7160c35aaf114a9d8fbefc32257dca21765bbb5
giedrius200 <48772520+giedrius200@users.noreply.github.com>
2021-09-17 09:24:37 +0300

    Update hashas.cpp

--- hashas.cpp
+++ hashas.cpp
@@ -3,10 +3,57 @@
 #include <sstream>
 #include <iomanip>
 
+#include <vector>
+#include <bitset>
+
 using namespace std;
+
+template<size_t N>
+std::string bitset_to_string(std::bitset<N> bits) {
+	static_assert(N % CHAR_BIT == 0, L);
+	std::string toReturn;
+	for (size_t j = 0; j < N / CHAR_BIT; ++j)
+	{
+		char next = 0;
+		for (size_t i = 0; i < CHAR_BIT; ++i)
+		{
+			size_t index = N - (CHAR_BIT * j) - i - 1;
+			size_t pos = CHAR_BIT - i - 1;
+			if (bits[index])
+				next |= (1 << pos);
+		}
+		toReturn.push_back(next);
+	}
+	return toReturn;
+}
 
 string paverstihex(string& s, bool upper_case)
 {
+		ostringstream ret;
+
+		string pridedas = s;
+		string simbolis = "";
+		string tuscia = "";
+
+		for (int i = 0; i < 256; i++) {
+			s.append(pridedas);
+		}
+		for (int i = 0; i < s.length(); ++i) {
+			bitset<64> bs4(s[i]);
+			bs4.flip();
+			simbolis = bitset_to_string(bs4);
+			tuscia.append(simbolis);
+		}
+		pridedas.append(tuscia);
+		s = pridedas;
+		cout << s.length() << endl;
+		if (s.length() > 256) {
+			while (s.length() != 256) {
+				s.pop_back();
+			}
+		}
+		cout << s.length() << endl;
+	
 		static const char hex_skaiciai[] = "0123456789ABCDEF";
 
 		std::string output;
